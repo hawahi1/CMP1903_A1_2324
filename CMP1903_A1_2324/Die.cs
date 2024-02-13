92cb349ad2ff48d3c1fb3a890755fd4b5269bfd7
elmotron <128416298+hawahi1@users.noreply.github.com>
2024-02-13 22:18:01 +0000

    Laid base, struggling to get value to print to check if assigned. Will get back to this in due time. Managed to iron out all problems with the code, and it builds successfully.

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,16 +9,26 @@ namespace CMP1903_A1_2324
 {
     internal class Die
     {
-        /*
-         * The Die class should contain one property to hold the current die value,
-         * and one method that rolls the die, returns and integer and takes no parameters.
-         */
 
         //Property
 
+        int dieValue;
+        Random rnd = new Random();
 
         //Method
 
+        public static void main(Random rnd, int dieValue)
+        {
+            int dieRoll = rnd.Next(1, 7);
+            dieValue = dieRoll;
+        }
 
+    }
+    internal class Print
+    {
+        public static void main(int dieValue)
+        {
+            Console.WriteLine(dieValue);
+        }
     }
 }

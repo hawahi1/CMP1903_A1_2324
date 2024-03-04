dd7e896ea4607aaec1738282ed686fa253ff68fd
hawahi <128416298+hawahi1@users.noreply.github.com>
2024-03-04 16:06:21 +0000

    Modified code
    Overhauled Die.cs so that it actually works, started writing Game.cs and now need to make the class print the die values.

--- CMP1903_A1_2324/Die.cs
+++ CMP1903_A1_2324/Die.cs
@@ -9,13 +9,18 @@ namespace CMP1903_A1_2324
     internal class Die
     {
         // Property
-        int DieValue
+        public int DieValue // creates a property to store the die value
         {
-            set { value = dieRoll; }
+            set; get;
         }
         // Method
-        static Random rnd = new Random();
-        int dieRoll = rnd.Next(1, 7);
+        static Random rnd = new Random(); // creates a random object which we use to generate random integers
+        public int dieRoll() // creates an integer object that stores the value of the die roll
+        {
+            DieValue = rnd.Next(1, 7); // effectively rolls the die, using the random objet to pick a number between 1 and 6
+            return DieValue; // returns and therefore stores the value of the die
+        }
+
 
     }
 }
--- CMP1903_A1_2324/Game.cs
+++ CMP1903_A1_2324/Game.cs
@@ -7,26 +7,38 @@ using System.Threading.Tasks;
 namespace CMP1903_A1_2324
 {
     internal class Game
+
+    // Creating Die Objects
     {
-        /*
-         * The Game class should create three die objects, roll them, sum and report the total of the three dice rolls.
-         *
-         * EXTRA: For extra requirements (these aren't required though), the dice rolls could be managed so that the
-         * rolls could be continous, and the totals and other statistics could be summarised for example.
-         */
+        Die dieOne = new Die();
+        Die dieTwo = new Die();
+        Die dieThree = new Die(); // Creating 3 die objects from the Die class in order to call their functions
 
-        //Methods
+        // Rolling the Die
 
-        static void Main(Die)
+        public int[] dieRolls()
         {
-            int dieOne = Die.dieRoll();
-            int dieTwo = Die.dieRoll();
-            int dieThree = Die.dieRoll();
-            int dieSum = dieOne + dieTwo + dieThree;
-            return dieSum;
-            Console.WriteLine(dieSum);
+            int rollOne = dieOne.dieRoll();
+            int rollTwo = dieTwo.dieRoll();
+            int rollThree = dieThree.dieRoll();
+            return new int[] { rollOne, rollTwo, rollThree };
         }
 
+        // Calculating the sum of the die
 
+        public int DieSum()
+        {
+            return dieRolls().Sum();
+        }
+
+        // Printing the die values and the sum of the die
+
+        public void diePrint()
+        {
+            Console.WriteLine($"Die number one is: ");
+            Console.WriteLine($"Die number two is: ");
+            Console.WriteLine($"Die number three is: ");
+            Console.WriteLine($"The sum is: ");
+        }
     }
 }
